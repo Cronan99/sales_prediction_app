872352cf81d9604479bee8bbefd3a8d7dd009e64
Cronan99 <albincronmark@gmail.com>
2026-09-08 13:50:11 +0200

    First models tests

--- main.py
+++ main.py
@@ -8,5 +8,15 @@ df = create_baseline_predictions(df)
 
 metrics = evaluate_baseline(df)
 
+print(
+    df[
+        [
+            "date",
+            "product",
+            "amount",
+            "baseline_prediction",
+        ]
+    ].head(20)
+)
 print(f"Baseline MAE: {metrics['mae']:.2f}")
 print(f"Baseline RMSE: {metrics['rmse']:.2f}")
