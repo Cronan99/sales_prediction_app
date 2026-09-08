3339fa925416bdc8cb28e10a88e5a6a013581118
Cronan99 <albincronmark@gmail.com>
2026-09-08 13:44:08 +0200

    Fixed main to run the baseline test of model

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from src.data_loader import load_sales_data
+from src.model import create_baseline_predictions, evaluate_baseline
+
 
 df = load_sales_data("data/fishmonger_sales.csv")
 
-print(df.head())
-print(df.info())
-print(df.dtypes)
+df = create_baseline_predictions(df)
+
+metrics = evaluate_baseline(df)
+
+print(f"Baseline MAE: {metrics['mae']:.2f}")
+print(f"Baseline RMSE: {metrics['rmse']:.2f}")
